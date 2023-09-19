1b4b2a38957c165bb20da4df7126c98f1dac1274
enkiga <eenkiga@gmail.com>
2023-09-20 01:42:49 +0300

    Pages

---
+++ resources/views/addCar.blade.php
@@ -0,0 +1,12 @@
+@extends('components.layout')
+@section('title', 'Car Details')
+@php
+    $activePage = 'car-details';
+@endphp
+
+@section('content')
+    @include('components.navigationBar')
+
+
+    @include('components.footer')
+@endsection
