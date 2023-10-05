3db2510a8d70cf8cc6e353b91329772cdaea1ea5
enkiga <eenkiga@gmail.com>
2023-10-05 19:55:39 +0300

    Admin Pages

--- app/Http/Controllers/AdministrationController.php
+++ app/Http/Controllers/AdministrationController.php
@@ -6,7 +6,6 @@ use App\Models\Requests;
 use App\Models\Routes;
 use App\Models\User;
 use App\Models\Vehicle;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class AdministrationController extends Controller
@@ -40,7 +39,34 @@ class AdministrationController extends Controller
 
     public function users(): View
     {
-        return view('AdminPages.adminUsers');
+        // get all users and paginate them into 10 users per page
+        $users = User::paginate(10);
+
+        // from users full name get the last name and save it as surname
+        foreach ($users as $user) {
+            $nameParts = explode(' ', $user->name);
+            if (isset($nameParts[1])) {
+                $user->surname = $nameParts[1];
+            } else {
+                // Handle the case where there is no last name, e.g., only a single-word name.
+                $user->surname = '';
+            }
+        }
+
+        // get first and second name from users name and save them as otherNames
+        foreach ($users as $user) {
+            $nameParts = explode(' ', $user->name);
+            if (isset($nameParts[2])) {
+                $user->otherNames = $nameParts[0] . ' ' . $nameParts[2];
+            } else {
+                // Handle the case where there is no second name, e.g., only a single-word name.
+                $user->otherNames = $nameParts[0];
+            }
+        }
+
+        return view('AdminPages.adminUsers', [
+            'users' => $users,
+        ]);
     }
 
     public function vehicles(): View
--- resources/views/AdminPages/adminUsers.blade.php
+++ resources/views/AdminPages/adminUsers.blade.php
@@ -7,6 +7,99 @@
 @section('content')
     @include('AdminPages.adminComponents.adminNavBar')
 
+    <section class="text-gray-600 body-font">
+        <div class="error">
+            @if($errors->any())
+                <script>
+                    let errorMessage = ''
+
+                    @foreach($errors->all() as $error)
+                        errorMessage += '{{$error}}\n';
+                    @endforeach
+
+                    if (errorMessage !== '') {
+                        alert(errorMessage);
+                    }
+                </script>
+            @endif
+
+            @if(session()->has('error'))
+                <script>
+                    alert('{{session('error')}}');
+                </script>
+            @endif
+        </div>
+        <!-- component -->
+        <div class="container mx-auto flex px-5 py-12 flex-col items-center">
+            <div class="overflow-x-auto w-full">
+                <div
+                    class="flex items-center justify-center">
+                    <div class="w-full lg:w-5/6">
+                        <div class="bg-white shadow-md rounded my-6 overflow-auto">
+                            <table class="min-w-max w-full table-auto">
+                                <thead>
+                                <tr class="bg-secondary-400 text-white uppercase text-sm leading-normal">
+                                    <th class="py-3 px-6 text-left">ID</th>
+                                    <th class="py-3 px-6 text-left">Surname</th>
+                                    <th class="py-3 px-6 text-center">Name</th>
+                                    <th class="py-3 px-6 text-center">Email Address</th>
+                                    <th class="py-3 px-6 text-center">Mobile Number</th>
+                                </tr>
+                                </thead>
+                                <tbody class="text-gray-600 text-sm font-light">
+                                @if($users->isEmpty())
+                                    <tr>
+                                        <td colspan="5" class="py-5 text-center">
+                                            <span
+                                                class="text-gray-400 font-semibold uppercase">No Users in the system</span>
+                                        </td>
+                                    </tr>
+                                @else
+                                    @foreach($users as $user)
+                                        <tr class="border-b border-gray-200 hover:bg-primary-100">
+
+                                            <td class="py-3 px-6 text-left">
+                                                <div class="flex items-center">
+                                                    <span>{{$user->id}}</span>
+                                                </div>
+                                            </td>
+                                            <td class="py-3 px-6 text-left">
+                                                <div class="flex items-center">
+                                                    <span>{{$user->surname}}</span>
+                                                </div>
+                                            </td>
+                                            <td class="py-3 px-6 text-center">
+                                                <span>{{$user->otherNames}}</span>
+                                            </td>
+                                            <td class="py-3 px-6 text-center">
+                                                <span>{{$user->email}}</span>
+                                            </td>
+                                            <td class="py-3 px-6 text-center">
+                                                <span>{{$user->phone}}</span>
+                                            </td>
+
+                                        </tr>
+                                    @endforeach
+                                @endif
+                                </tbody>
+                            </table>
+                            {{--pageination--}}
+                            <div
+                                class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+                                <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+                                    <div class="space-x-2"> <!-- Add space-x-* class for spacing -->
+                                        {{ $users->links() }}
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
     @include('components.footer')
 @endsection
